5e747d6075563cb93a5498f0e97611f7afcde1fc
Lisa Fung <lisafung@stanford.edu>
2024-03-15 16:02:04 -0700

    Add soil moisture reading

---
+++ final_sensors/soil_moisture.c
@@ -0,0 +1,26 @@
+/* Soil Moisture Sensor Implementation
+ * Lisa Fung
+ */
+
+#include "gpio.h"
+#include "uart.h"
+#include "printf.h"
+
+static unsigned int mcp_channel;
+
+void soil_moisture_init(unsigned int channel) {
+    mcp3008_init();
+    mcp_channel = channel;    
+}
+
+// Read soil moisture AOUT through MCP3008 Channel 0
+int soil_moisture_read(void) {
+    // Values read when completely dry and completely in water
+    static const int DRY = 860, WATER = 400;
+    
+    int moisture = mcp3008_read(mcp_channel);
+    printf("Soil Moisture value read: %4d\n", moisture);
+    int moisture_percent = 100 * (DRY - moisture) / (DRY - WATER);
+    printf("Moisture: %2d%%\n", moisture_percent);
+    return moisture_percent;
+}
